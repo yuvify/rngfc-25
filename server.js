a98a0a2da715eca88c11bfc017474a37cf5f6e69
yuvify <yuvi396@outlook.com>
2024-09-24 20:10:08 -0400

    Add files via upload

---
+++ server.js
@@ -0,0 +1,28 @@
+const path = require("path");
+const fastify = require("fastify")({
+  logger: false,
+});
+
+fastify.register(require("@fastify/static"), {
+  root: path.join(__dirname, "public"),
+  prefix: "/",
+});
+
+fastify.get("/", function (request, reply) {
+  reply.sendFile("index.html");
+});
+
+fastify.get("/inventory", function (request, reply) {
+  reply.sendFile("inventory.html");
+});
+
+fastify.listen(
+  { port: process.env.PORT || 3000, host: "0.0.0.0" },
+  function (err, address) {
+    if (err) {
+      console.error(err);
+      process.exit(1);
+    }
+    console.log(`Your app is listening on ${address}`);
+  }
+);
